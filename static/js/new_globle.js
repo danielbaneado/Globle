ca3cafffcd20ac55c599cc0f2329bc5e25a1fff7
Ricardo031 <rjtb31@gmail.com>
2026-05-19 10:12:23 -0500

    fixed logic

--- static/js/new_globle.js
+++ static/js/new_globle.js
@@ -1,11 +1,11 @@
-import countries from '../json/countries_dict.json' with { type: 'json' };
-import flags from '../json/flags_dict.json' with { type: 'json' };
+import countries from '../json/countries_dict.json' with { type: 'json' }
+import flags from '../json/flags_dict.json' with { type: 'json' }
 const qualities= [
-    "Iniciales",
+    "Inicial(es)",
     "Colores de bandera",
     "Continente",
     "Vecinos",
-    "Dato"
+    "Facto"
 ]
 const countriesToGet = []
 for (const [name] of Object.entries(countries.countries)) {
@@ -24,84 +24,93 @@ function getRandomCountry() {
   const randomCountry= countriesToGet[index]
   return randomCountry
 }
+
 class Game {
-    constructor(guessBtn, skipBtn, hintTitle, hintContent, matchContent) {
+    constructor(guessBtn, skipBtn, hintTitle, hintContent, matchContent, hiddenTitle) {
         this.country= this.getCountry()
         this.points= 120
         this.attempts= 0
         this.guesses= []
+        this.flag= []
         this.guessBtn= guessBtn
         this.skipBtn= skipBtn
         this.hintTitle= hintTitle
         this.hintContent= hintContent
         this.matchContent= matchContent
+        this.hiddenTitle= hiddenTitle
     }
     showHints(qualities) {
         this.hintTitle.innerHTML= `${qualities[this.attempts - 1]}`
+        this.matchContent.classList.remove("hide-content")
+        this.hiddenTitle.classList.remove("hide-content")
         const mysteryCountry = countries.countries[Object.keys(countries.countries).find(c => c.toLocaleLowerCase() === this.country)]
         if(this.attempts=== 4) {
-          for(const neighbors of mysteryCountry[4]) {
+          this.hintContent.innerHTML= ""
+          for(const neighbors of mysteryCountry[3]) {
             this.hintContent.innerHTML+= `${neighbors}, `
           }
         }
-        else {
-          this.hintContent.innerHTML= `${qualities[this.attempts - 1]}: ${mysteryCountry[this.attempts - 1]}`
+        else if(this.attempts >= 6) {
+          this.hintContent.classList.add("hide-content")
+          this.hintTitle.classList.add("hide-content")
         }
+        else {
+          this.hintContent.innerHTML= `${mysteryCountry[this.attempts - 1]}`
+        }
+        
     }
     validateAttempt(guess, normalizedGuess) {
         if (!countriesToGet.includes(normalizedGuess)) {
-            alert("El país ingresado no es válido. Por favor, intenta con otro.")
+            alert("País inválido!")
             return
         }
         if (normalizedGuess=== this.country) {
-            alert("¡Correcto! Has ganado.")
-            this.points+= 20
+            alert(`El país misterioso era: ${this.country}`)
             alert(`Puntos obtenidos: ${this.points}`)
         }
         else {
             this.points-= 20
             this.attempts++
-            this.guesses.push(guess)
+            if (!this.guesses.includes(guess)) {  
+                this.guesses.push(guess)
+                this.flag.push(flags[normalizedGuess])
+            }
             if(this.attempts < 5) {
                 this.showHints(qualities)
-            }
-            else {
-                alert(`No adivinaste :( El país era: ${this.country}`)
             }
         }
     }
     attempt(guess, normalizedGuess) {
       if (countriesToGet.includes(normalizedGuess) && !this.guesses.includes(guess)) {
         this.validateAttempt(guess, normalizedGuess)
+        this.matchContent.innerHTML+= `<p>${guess.toLocaleUpperCase()}  </p>`
       }
-      else {
-        this.matchContent.innerHTML+= `<p>${guess}</p>`
-        }
     }
 }
 class DailyGame extends Game {
-    constructor(guessBtn, skipBtn, hintTitle, hintContent, matchContent) {
-        super(guessBtn, skipBtn, hintTitle, hintContent, matchContent)
+    constructor(guessBtn, skipBtn, hintTitle, hintContent, matchContent, hiddenTitle) {
+        super(guessBtn, skipBtn, hintTitle, hintContent, matchContent, hiddenTitle)
     }
     getCountry() {
         return getDailyCountry()
     }
 }
 class RandomGame extends Game {
-    constructor(guessBtn, skipBtn, hintTitle, hintContent, matchContent) {
-        super(guessBtn, skipBtn, hintTitle, hintContent, matchContent)
+    constructor(guessBtn, skipBtn, hintTitle, hintContent, matchContent, hiddenTitle) {
+        super(guessBtn, skipBtn, hintTitle, hintContent, matchContent, hiddenTitle)
     }
     getCountry() {
         return getRandomCountry()
     }
 }
 document.addEventListener("DOMContentLoaded", () => {
-  const dailyGame = new DailyGame(
+  const dailyGame= new DailyGame(
     document.getElementById("guess-btn"),
     document.getElementById("skip-btn"),
     document.getElementById("hint-number"),
     document.getElementById("hint-content"),
-    document.getElementById("match-content")
+    document.getElementById("match-content"),
+    document.getElementById("hidden-title")
   );
 
   dailyGame.guessBtn.addEventListener("click", (e) => {
@@ -109,10 +118,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const guess= document.getElementById("country-input").value
     const normalizedGuess= guess.toLocaleLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
     dailyGame.attempt(guess, normalizedGuess)
-  });
+  })
 
   dailyGame.skipBtn.addEventListener("click", () => {
     dailyGame.attempts++
     dailyGame.showHints(qualities)
-  });
-});
+  })
+})
---
+++ static/js/random.js
@@ -0,0 +1,1 @@
+import globle from './script.js'
