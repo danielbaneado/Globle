5e7230a3b839279b834b315e29aadf7658d1bf61
Ricardo031 <rjtb31@gmail.com>
2026-05-21 16:40:32 -0500

    finished infinite mode

--- static/js/random.js
+++ static/js/random.js
@@ -14,29 +14,40 @@ class RandomGame extends Game {
     getCountry() {
         return getRandomCountry()
     }
+    playAgain(){
+        return match()
+    }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const randomGame= new RandomGame(
-    document.getElementById("guess-btn"),
-    document.getElementById("skip-btn"),
-    document.getElementById("hint-number"),
-    document.getElementById("hint-content"),
-    document.getElementById("match-content"),
-    document.getElementById("hidden-title"),
-    document.getElementById("messages"),
-  )
-  const countryForm= document.getElementById("country-form")
-  randomGame.guessBtn.addEventListener("click", (e) => {
-    e.preventDefault()
-    const guess= document.getElementById("country-input").value
-    const normalizedGuess= guess.toLocaleLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-    countryForm.reset()
-    randomGame.attempt(guess, normalizedGuess)
-  })
-  
-  randomGame.skipBtn.addEventListener("click", () => {
-    randomGame.attempts++
-    randomGame.showHints(qualities)
-  })
+function match(){
+    document.addEventListener("DOMContentLoaded", () => {
+        const randomGame= new RandomGame(
+            document.getElementById("guess-btn"),
+            document.getElementById("skip-btn"),
+            document.getElementById("hint-number"),
+            document.getElementById("hint-content"),
+            document.getElementById("match-content"),
+            document.getElementById("hidden-title"),
+            document.getElementById("messages"),
+        )
+        const countryForm= document.getElementById("country-form")
+        randomGame.guessBtn.addEventListener("click", (e) => {
+            e.preventDefault()
+            const guess= document.getElementById("country-input").value
+            const normalizedGuess= guess.toLocaleLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+            countryForm.reset()
+            randomGame.attempt(guess, normalizedGuess)
+        })
+        
+        randomGame.skipBtn.addEventListener("click", () => {
+            randomGame.attempts++
+            randomGame.showHints(qualities)
+        })
+    })
+}
+const newGame= document.getElementById("new-game")
+newGame.addEventListener("click", () => {
+    location.reload()
+    match()
 })
+match()
